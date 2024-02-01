375bebfa76ac842e27e175b2597b21d1c03dbd9d
Hxrshmxllow <86836424+Hxrshmxllow@users.noreply.github.com>
2024-02-01 16:17:29 +0000

    yes

---
+++ Chess/board.py
@@ -0,0 +1,15 @@
+import pygame as pygame
+
+screen = pygame.display.set_mode((300, 300)) 
+
+running = True
+  
+# game loop 
+while running: 
+    
+# for loop through the event queue   
+    for event in pygame.event.get(): 
+      
+        # Check for QUIT event       
+        if event.type == pygame.QUIT: 
+            running = False
