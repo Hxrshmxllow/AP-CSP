7c5befce4f50cebb2e2f3f072e4c7912d77a7a19
Hxrshmxllow <86836424+Hxrshmxllow@users.noreply.github.com>
2024-02-20 16:38:21 +0000

    yes

---
+++ Create Task/Calculator.py
@@ -0,0 +1,36 @@
+import re
+
+numbers = []
+actions = []
+numberregex = r'[0-9]'
+
+equation = input("Enter the equation you want to solve: ")
+
+for number in equation.split(" "):
+    if number.isdigit():
+        numbers.append(int(number))
+
+action = re.sub(numberregex, "", equation).replace(" ", "")
+for x in action:
+    actions.append(x)
+
+answer = 0
+while len(numbers) != 1:
+    while len(actions) != 0:
+        if action[0] == "+":
+            answer += numbers[0] + numbers[1]
+        elif action[0] == "-":
+            answer += numbers[0] - numbers[1]
+        elif action[0] == "/":
+            answer += numbers[0] / numbers[1]
+        elif action[0] == "*":
+            answer += numbers[0] * numbers[1]
+        actions.pop(0)
+        numbers.pop(0)
+        if len(numbers) != 1:
+            numbers[0] = answer
+        print(answer)
+        print(actions)
+        print(numbers)
+
+print(f"Answer: {answer}")
